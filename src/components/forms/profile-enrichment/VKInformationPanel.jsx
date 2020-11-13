cf76341dd5e1b7f3e4e4e1bf2583aa78acdeaaa8
Anatoly Loskutov <tly-90@yandex.ru>
2020-11-13 17:42:23 +0300

    webstorm commit settings

--- src/components/forms/profile-enrichment/VKInformationPanel.jsx
+++ src/components/forms/profile-enrichment/VKInformationPanel.jsx
@@ -3,19 +3,17 @@ import { has, get } from "lodash";
 
 export const VKInformationPanel = ({ profileInDB }) => {
   if (has(profileInDB, "hasProfileCheckRow")) {
-    return (
-      <InformationBlock optionalComment={'Профайл есть в БД'}
-      />
-    );
+    return <InformationBlock optionalComment={"Профайл есть в БД"} />;
   } else if (has(profileInDB, "hasProfileRow")) {
     return (
-      <InformationBlock optionalComment={'Профайл есть только в одной БД'}
-      />
+      <InformationBlock optionalComment={"Профайл есть только в одной БД"} />
     );
   }
   return <div>Не обнаружено записей в БД по данному профайлу</div>;
 };
 
-const InformationBlock = ({  optionalComment = '' }) => {
-  return <div className={'profile-info-label'}>{optionalComment}</div>;
+const InformationBlock = ({ optionalComment = "" }) => {
+  return <div className={"profile-info-label"}>{optionalComment}</div>;
 };
+
+const a = 1;
