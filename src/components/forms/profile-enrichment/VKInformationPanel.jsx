8662d88746c7ea6a60ff998a384b3276d77d3f0a
Anatoly Loskutov <tly-90@yandex.ru>
2020-11-17 13:43:03 +0300

    presentation component developing

--- src/components/forms/profile-enrichment/VKInformationPanel.jsx
+++ src/components/forms/profile-enrichment/VKInformationPanel.jsx
@@ -2,19 +2,14 @@ import React from "react";
 import { has } from "lodash";
 import { getBEMClassName } from "../../utils/bem-helper";
 import { Grid, Icon, Table, Menu, Rating } from "semantic-ui-react";
+import { setMaxWidth, setWidth } from "../../utils/css-class-util";
 
 export const VKInformationPanel = ({ profileInDB }) => {
-  if (has(profileInDB, "hasProfileCheckRow")) {
-    return <InformationProperty optionalComment={"Профайл есть в БД"} />;
-  } else if (has(profileInDB, "hasProfileRow")) {
-    return (
-      <InformationProperty optionalComment={"Профайл есть только в одной БД"} />
-    );
-  }
-  return <div>Не обнаружено записей в БД по данному профайлу</div>;
+
+  return <InformationProperty profileInDB={profileInDB}  />;
 };
 
-const InformationProperty = ({ optionalComment = "" }) => {
+const InformationProperty = ({ profileInDB }) => {
   return (
     <Grid columns={2} padded>
       <Grid.Row>
@@ -31,10 +26,12 @@ const InformationProperty = ({ optionalComment = "" }) => {
             <Table.Body>
               <Table.Row textAlign="center">
                 <Table.Cell>
-                  <Icon name="checkmark" size="big" color={"green"}></Icon>
+                  {profileInDB.hasProfileRow ? <Icon name="checkmark" size="big" color={"green"}></Icon>
+                      : <Icon name="cancel" size="big" color={"red"}></Icon>}
                 </Table.Cell>
                 <Table.Cell>
-                  <Icon name="cancel" size="big" color={"red"}></Icon>
+                  {profileInDB.hasProfileCheckRow ? <Icon name="checkmark" size="big" color={"green"}></Icon>
+                      : <Icon name="cancel" size="big" color={"red"}></Icon>}
                 </Table.Cell>
               </Table.Row>
             </Table.Body>
@@ -59,28 +56,29 @@ const InformationProperty = ({ optionalComment = "" }) => {
           </React.Fragment>
         </Grid.Column>
         <Grid.Column>
-          <RatingComponent value={0}/>
+          <RatingComponent name={'общая'} value={profileInDB.estimation}/>
         </Grid.Column>
       </Grid.Row>
     </Grid>
   );
 };
 
-const MenuFragment = ({ visible = false, modifier = "" }) => {
+const MenuFragment = ({ visible = false, modifiers = [] }) => {
   return visible ? (
     <div
       className={getBEMClassName("VKInformationPanel", "menuSegment", [
         "m-position-rel",
         "m-text-align-center",
       ])}
+      style={{...setWidth('500px'), ...setMaxWidth('1000px')}}
     >
       Segment Menu
     </div>
   ) : false;
 };
 
-const RatingComponent = ({optionalComment = '', value = 0}) => {
+const RatingComponent = ({name = '', value = 0}) => {
   return (
-      <div>Оценка {optionalComment}: <Rating maxRating={10} clearable icon={'star'} defaultRating={value}/> ({value})</div>
+      <div>Оценка {name}: <Rating maxRating={10} clearable icon={'star'} defaultRating={parseInt(value, 10)}/> ({value})</div>
   )
 }
--- src/components/utils/css-class-util.js
+++ src/components/utils/css-class-util.js
@@ -18,3 +18,34 @@ const makeCSSRuleByPropName = (propNameInCamelCase, value) => {
 const createModifierName = (key, value) => {
     return key + '_' + value;
 }
+
+export const setWidth = (value = null) => {
+    return {
+        width: value
+    }
+}
+export const setLeft = (value = null) => {
+    return {
+        left: value
+    }
+}
+
+export const setRight = (value = null) => {
+    return {
+        right: value
+    }
+}
+
+export const setTop = (value = null) => {
+    return {
+        top: value
+    }
+}
+
+export const setMaxWidth = (value = null) => {
+    return {
+        maxWidth: value
+    }
+}
+
+
