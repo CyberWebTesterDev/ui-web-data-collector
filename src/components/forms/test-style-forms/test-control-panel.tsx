11bdb33ba51b380be66b8e9c5db09cb23a3d1c32
Anatoly Loskutov <tly-90@yandex.ru>
2021-05-20 23:10:04 +0300

    move to ts, fixes, and refactor

--- src/components/forms/test-style-forms/test-control-panel.tsx
+++ src/components/forms/test-style-forms/test-control-panel.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import './control-panel.css';
+import "./control-panel.css";
 
 export const TestControlPanel = React.memo<undefined>(() => {
    let btnArrExample: TButton[] = [
